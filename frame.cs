2d8c9b08deddd1565f9cd8e88defa8c3fd44ed2f
Abinayasrinivasan305 <abinayasrinivasan2004@gmail.com>
2024-07-19 18:10:39 +0530

    committed

---
+++ frame.cs
@@ -0,0 +1,33 @@
+.div{
+  height:100%;
+  margin:0px;
+  background-color:black;
+  border-radius:7px;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:space-around;
+  width:250px;
+  
+}
+.a{
+ font-size:20px;
+ font-weight:bold;
+ font-family:arial;
+ color:white;
+}
+a:link{
+
+ 
+text-decoration:none;
+}
+a:hover{
+ color:blue;
+ padding:5px 70px 5px 70px;
+ border-radius:5px;
+ background-color:white;
+}
+.k1{
+font-size:15px;
+
+}
